71e4a335cbc3e942f0274997e6b6dea5b96ccf6a
ssmeds <stina_smeds@hotmail.com>
2021-03-25 10:21:28 +0100

    created first files

---
+++ script.js
@@ -0,0 +1,13 @@
+// Bondalizer
+// My name is Bond, James Bond!
+
+// Uppgiften
+// Skapa en kontaktlista (object array) med minst 4 personer. Den skall innehålla minst för och efternamn som egna key-pair-values.
+
+// Använd prompt() eller skapa ett formulär och en knapp via vilket besökaren kan skriva i ett förnamn på en person. Sök i listan om du hittar en träff. I så fall skriv ut på sidan eller i konsollen en hälsning: "His name is Efternamn, Förnamn Efternamn!". Hittar du ingen träff skriv istället "Agent not found!".
+
+// Pseudokod
+// Skriv först uppgiften som pseudokod. Hur skulle flödet för applikationen se ut? Vilka steg och uppgifter behöver du göra?
+
+// Lösningen
+// Lös uppgiften med riktig kod!
